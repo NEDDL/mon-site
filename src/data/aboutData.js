3c5754c14f43d3d8d3b0d43411a3e21e17bc10cd
Mustafa ONAL <mustafasaitonal@outlook.com>
2022-07-06 16:55:07 +0200

    CV

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,7 +2,7 @@ export const AboutData = {
   image: "/images/me.jpg",
   about: [
     "Hello! My name is Mustafa and I enjoy creating things that live on the internet. My interest in web development started back in 2014 when I decided to try editing WordPress themes — turns out customizing themes taught me a lot about HTML & CSS!",
-    "Fast-forward to today, I've had experience in a startup, huge financial corporations and even collaborated starting a new business. My main focus these days is building inclusive products and digital experiences at Soleilex.",
+    "Fast-forward to today, I've had experience in a startup, huge financial corporations and even collaborated starting a new business. Currently I am a Freelance Full Stack Developer with MERN, FERN stacks. Feel free to contact me if you have projects to discuss!",
     "I am also launching little apps while practicing different languages and stack technologies.",
     "Here are some certificates that I obtained: ",
   ],
